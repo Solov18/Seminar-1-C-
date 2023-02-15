dbf9cd6c19f3b354cd5ef4efce09a5394066d1a5
Solov18 <solov18@list.ru>
2023-02-15 09:53:02 +0300

    мах число

---
+++ Домашка/Дз № 1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+int number1, number2;
+Console.Write("Введите 1 число: ");
+int.TryParse(Console.ReadLine()!, out number1);
+Console.Write("Введите 2 число: ");
+int.TryParse(Console.ReadLine()!, out number2);
+if (number1 > number2)
+    Console.Write($"{number1}");  
+else 
+    Console.Write($"{number2}"); 
+
