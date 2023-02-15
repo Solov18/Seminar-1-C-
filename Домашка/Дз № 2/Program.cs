aa3f89215e0a98e64e42da13141b3f4baa826d5f
Solov18 <solov18@list.ru>
2023-02-15 10:31:59 +0300

    dz 2

--- Домашка/Дз № 2/Program.cs
+++ Домашка/Дз № 2/Program.cs
@@ -4,3 +4,15 @@
 44 5 78 -> 78
 22 3 9 -> 22 */
 
+int number1, number2, number3;
+Console.Write("Введите 1 число: ");
+int.TryParse(Console.ReadLine()!, out number1);
+Console.Write("Введите 2 число: ");
+int.TryParse(Console.ReadLine()!, out number2);
+Console.Write("Введите 3 число: ");
+int.TryParse(Console.ReadLine()!, out number3);
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+    Console.Write($"{max}");  
+
