3275b32843d7840e7ebf269489bdffebfb6d810b
Solov18 <solov18@list.ru>
2023-02-15 11:10:37 +0300

    dz 3

--- Домашка/Дз № 3/Program.cs
+++ Домашка/Дз № 3/Program.cs
@@ -1,14 +1,10 @@
 ﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
- (делится ли оно на два без остатка). */
+ (делится ли оно на два без остатка). 
+ 4 -> да
+-3 -> нет
+ 7 -> нет */
 
-int number1, number2, number3;
+int number1;
 Console.Write("Введите 1 число: ");
 int.TryParse(Console.ReadLine()!, out number1);
-Console.Write("Введите 2 число: ");
-int.TryParse(Console.ReadLine()!, out number2);
-Console.Write("Введите 3 число: ");
-int.TryParse(Console.ReadLine()!, out number3);
-int max = number1;
-if (number2 > max) max = number2;
-if (number3 > max) max = number3;
-    Console.Write($"{max}"); 
+Console.WriteLine(number1 % 2 == 0 ? "Четное" : "Не четное");
